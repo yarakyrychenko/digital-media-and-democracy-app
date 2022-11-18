a20354e47e67317c79dd723d0ada6b148cb83293
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-11-18 12:32:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ effects.insert(0, "All")
 # Abstract.Note for data_review
 data["text"] = [str(data.loc[i, "Title"]) + " " + str(data.loc[i, "Abstract Note...8"]) for i in range(len(data))]
 
-lottie_tweet = load_lottieurl('https://assets3.lottiefiles.com/packages/lf20_t2xm9bsw.json')
+lottie_tweet = load_lottieurl('https://assets6.lottiefiles.com/packages/lf20_tnrzlN.json')
 st_lottie(lottie_tweet, speed=1, height=200, key="initial")
 
 st.markdown("<h1 style='text-align: center;'> Digital Media and Democracy </h1>", unsafe_allow_html=True)
