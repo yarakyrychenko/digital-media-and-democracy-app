9f4fc549857eb24a8c70e377700d91632ceddb0a
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-11-20 15:28:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ overtime  = overtime[overtime["country"] != "Unknown"]
 overtime  = overtime[overtime["country"] != "World"]
 overtime["not selected"] = overtime["country"].apply( lambda country: 1 if country not in set(st.session_state.COUNTRY) else 0 )
 overtime["selected"] = overtime["country"].apply( lambda country: 1 if country in set(st.session_state.COUNTRY) else 0 )
-st.write(overtime[["Year","selected","not selected"]]))
+st.write(overtime[["Year","selected","not selected"]])
 st.line_chart(overtime[["Year","selected","not selected"]].groupby('Year').agg('sum'))
 
 st.slider("How many titles would you like to explore?", min_value=0, max_value=len(df), value= 10 if len(df) > 9 else len(df) , step=1, key="number_to_print")
